dfb697eb74af4e76e22193c6a763abc529ad3bc6
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-20 04:13:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,13 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRAND = []
+  BRANDS = []
   
   def initialize(brand)
     @brand = brand
-    if BRAND.include?(brand)
+    if BRANDS.include?(brand)
     else 
-    BRAND << brand
+    BRANDS << brand
   end 
   end
 
